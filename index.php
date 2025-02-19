48f1fef088633de95c0f1d5cea8f33b0a30b8b5e
topikopek <topikcool4@gmail.com>
2025-02-19 16:04:00 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, facilis! Facere mollitia quod atque numquam, deserunt tempore optio consequuntur! Laboriosam quis dolorem nihil. Eos tenetur harum voluptas esse facere voluptates!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
