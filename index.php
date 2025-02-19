ea7f511d4095b1ffdb523bc032bb7c1032cef230
topikopek <topikcool4@gmail.com>
2025-02-19 15:53:23 +0800

    menambahkan h1 dan paragraf

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Coba Git Remote</title>
 </head>
 <body>
-    
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, facilis! Facere mollitia quod atque numquam, deserunt tempore optio consequuntur! Laboriosam quis dolorem nihil. Eos tenetur harum voluptas esse facere voluptates!</p>
 </body>
 </html>
